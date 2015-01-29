e8b4272f394b30732b0f8538dce33efb6f52b987
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-01-29 10:49:08 -0800

    sets up remaining tables and classes

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129180338) do
+ActiveRecord::Schema.define(version: 20150129184554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
