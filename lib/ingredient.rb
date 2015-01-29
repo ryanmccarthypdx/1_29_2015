0698733322ded1dae4db27952e8418a2f128bbf3
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-01-29 12:24:34 -0800

    No, seriously, this one fixes all validation and callback issues, or so we think right now

--- lib/ingredient.rb
+++ lib/ingredient.rb
@@ -1,5 +1,13 @@
 class Ingredient < ActiveRecord::Base
   has_and_belongs_to_many(:recipes)
   validates_uniqueness_of(:name, {:case_sensitive => false})
+  before_save(:titlecase_name)
 
+  scope :servings?, lambda {|serving_request|
+    where("SERVINGS >= ?", serving_request)}
+
+private
+  define_method(:titlecase_name) do
+    self.name=(name().titlecase())
+  end
 end
