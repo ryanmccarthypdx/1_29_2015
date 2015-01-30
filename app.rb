2af5786af56d0d486a94f148c1a80239413a3105
Ryan McCarthy <ryanmccarthypdx@gmail.com>
2015-01-29 16:00:48 -0800

    Add ability to associate ingredients and recipies

--- app.rb
+++ app.rb
@@ -8,3 +8,33 @@ get('/') do
   @title = "Cookbook"
   erb(:index)
 end
+
+post('/recipe_add') do
+  name_input = params.fetch("new_recipe_name")
+  new_recipe = Recipe.create({:name => name_input})
+  @recipe_name = new_recipe.name()
+  @recipe_id = new_recipe.id()
+  @title = @recipe_name
+  @list_of_ingredients = Ingredient.all()
+  @list_of_categories = Category.all()
+  erb(:recipe_edit)
+end
+
+post('/ingredient_add') do
+  @list_of_ingredients = Ingredient.all()
+  @recipe_id = (params.fetch("recipe_id")).to_i()
+  @recipe_ingredients = Recipe.find(@recipe_id).ingredients()
+  @recipe_name = (params.fetch("recipe_name"))
+  new_ingredient_name = params.fetch('new_ingredient_name')
+  if new_ingredient_name != ""
+    Ingredient.create({:name => new_ingredient_name, :recipe_ids => @recipe_id})
+  else
+    old_ingredient_id = params.fetch('ingredient_id')
+    old_ingredient = Ingredient.find(old_ingredient_id)
+    old_ingredient.update({:recipe_ids => [@recipe_id]})
+  end
+  @title = @recipe_name
+
+  @list_of_categories = Category.all()
+  erb(:recipe_edit)
+end
